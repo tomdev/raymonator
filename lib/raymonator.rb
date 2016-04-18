eeb4197d2830b1c0d2b23e176ce3ea950d6b6883
Tom de Vries <tom@hackerone.com>
2016-04-18 13:20:38 +0200

    Implementation and executable

--- lib/raymonator.rb
+++ lib/raymonator.rb
@@ -1,5 +1,29 @@
 require "raymonator/version"
 
 module Raymonator
-  # Your code goes here...
+  class WordCompiler
+    def raymonate
+      puts "#{first_word}#{last_word}".capitalize
+    end
+
+    private
+
+    def first_word
+      %w(
+        dak
+        baksteen
+        zakke
+      ).sample
+    end
+
+    def last_word
+      %w(
+        dekker
+        wasser
+        flapper
+        nakker
+        baffer
+      ).sample
+    end
+  end
 end
